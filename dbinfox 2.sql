690ef90aab93669ade7d49aeac02bb9dd447f516
LucasZXS <38111076+LucasZXS@users.noreply.github.com>
2018-07-31 16:57:23 -0300

    Mudança adiciando relatório

--- dbinfox 2.sql
+++ dbinfox 2.sql
@@ -50,6 +50,55 @@ insert into tb_cliente values
 
 select * from tb_cliente;
 
+drop table tb_cliente;
+ 
+create table tb_os(
+idos int auto_increment primary key,
+tipo1 varchar(15) not null,
+situacao varchar(20) not null,
+equipamento varchar(200) not null,
+defeito varchar(200) not null,
+servico varchar(200),
+tecnico varchar(200),
+valor decimal(10,2)
+);
+describe tb_os;
+select * from tb_os;
+describe tb_cliente;
+select * from tb_cliente;
+
+insert into tb_os (tipo1,situacao,equipamento,defeito,servico,valor,idcli) values
+('Conserto','Aprovado','Desktop antigo','não liga','trocar fonte',300,2);
+
+insert into tb_os (tipo1,situacao,equipamento,defeito,servico,valor,idcli) values
+('Orçamento','Aprovado','Notebook lenovo','Pouca RAM','RAM Upgrade',400,1);
+
+insert into tb_os (tipo1,situacao,equipamento,defeito,servico,valor,idcli) values
+('Conserto','Reprovado','Notebook antigo','Listras na tela','Consertar o LED',900,3);
+
+alter table tb_os auto_increment = 10000;
+
+alter table tb_os add idcli int;
+
+alter table tb_os drop column idcli;
+
+alter table tb_os add constraint cliente_os
+foreign key(idcli)
+references tb_cliente(idcli)
+on delete no action;
+
+alter table tb_os add dataos timestamp default current_timestamp
+after idos;
+
+-- relatorio
+
+select O.idos as OS,tipo1 as Tipo,situacao as Situação,equipamento as Equipamento,defeito as Defeito,
+servico as Serviço,tecnico as Tecnico,valor as Valor,dataos as Dataos
+C.nomecli as Nome, fone1 as Fone, emailcli as Email
+from tb_os as O
+inner join tb_cliente as C on (O.idcli = C.idcli);
+
+
 
 
 
